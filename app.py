b902c2d7d64c943d088ea0028d3fb0338cb177d5
arank <arankhanna@college.harvard.edu>
2015-04-07 15:02:15 -0400

    modified flask server to return json with last location visited

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!flask/bin/python
 from flask import Flask
+import pymongo, ConfigParser
 
 app = Flask(__name__)
 
-@app.route('/')
+config = ConfigParser.RawConfigParser()
+config.read('passwords.cfg')
+MONGODB_URI = config.get('mongo', 'uri')	
+
+@app.route('/last-location')
 def index():
-    return "Hello, World!"
+    client = pymongo.MongoClient(MONGODB_URI)
+	db = client.get_default_database()
+	creds = db['daily_location'].find_one()
+	return str(creds)
 
 if __name__ == '__main__':
     app.run(debug=True)
